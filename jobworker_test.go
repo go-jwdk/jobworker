4b9176497f625771bae40a92394c9d1830a2a3a4
Yuichi Watanabe <yuichi.watanabe.ja@gmail.com>
2020-03-16 20:22:22 +0900

    add a unit test for fail job func

--- jobworker_test.go
+++ jobworker_test.go
@@ -48,11 +48,88 @@ func Test_completeJob(t *testing.T) {
 			},
 			wantErr: true,
 		},
+		{
+			name: "already finish",
+			args: args{
+				ctx: context.Background(),
+				job: &Job{
+					QueueName:    "Foo",
+					didSomething: 1,
+				},
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := completeJob(tt.args.ctx, tt.args.job); (err != nil) != tt.wantErr {
 				t.Errorf("completeJob() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if ok := tt.args.job.IsFinished(); ok != !tt.wantErr {
+				t.Errorf("IsFinished() result = %v, wantErr %v", ok, !tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_failJob(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		job *Job
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "normal case",
+			args: args{
+				ctx: context.Background(),
+				job: &Job{
+					Conn: &ConnectorMock{
+						FailJobFunc: func(ctx context.Context, input *FailJobInput) (output *FailJobOutput, e error) {
+							return &FailJobOutput{}, nil
+						},
+					},
+					QueueName:    "Foo",
+					didSomething: 0,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "error case",
+			args: args{
+				ctx: context.Background(),
+				job: &Job{
+					Conn: &ConnectorMock{
+						FailJobFunc: func(ctx context.Context, input *FailJobInput) (output *FailJobOutput, e error) {
+							return &FailJobOutput{}, errors.New("mock error")
+						},
+					},
+					QueueName:    "Foo",
+					didSomething: 0,
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "already finish",
+			args: args{
+				ctx: context.Background(),
+				job: &Job{
+					QueueName:    "Foo",
+					didSomething: 1,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := failJob(tt.args.ctx, tt.args.job); (err != nil) != tt.wantErr {
+				t.Errorf("failJob() error = %v, wantErr %v", err, tt.wantErr)
 			}
 			if ok := tt.args.job.IsFinished(); ok != !tt.wantErr {
 				t.Errorf("IsFinished() result = %v, wantErr %v", ok, !tt.wantErr)
